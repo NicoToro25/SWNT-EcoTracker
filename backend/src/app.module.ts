2f167cadaf5fb9a86d3ade31b1670c741ff85a7e
NicoToro25 <nikotoro25@gmail.com>
2026-03-06 01:11:41 -0500

    Serve frontend with NestJS

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { FootprintModule } from './footprint/footprint.module';
 
 @Module({
-  imports: [FootprintModule],
+  imports: [
+    FootprintModule,
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../frontend/dist'),
+    }),
+  ],
 })
 export class AppModule {}
